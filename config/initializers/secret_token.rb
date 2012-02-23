c759db1b8c185c5d02f720a4ed5b40867e753618
Michael Slone <m.slone@gmail.com>
2012-02-23 08:50:49 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PresenceBlacklight::Application.config.secret_token = '06f9bdb0f61b29a4e27dbffa494a13d34ae70f8eab7d5e18db423fb3fe6a26a756c4a3d9bf46403c0a929e404e7c92d9f52a12950f6d6a9ae9cad26d22f5c017'
